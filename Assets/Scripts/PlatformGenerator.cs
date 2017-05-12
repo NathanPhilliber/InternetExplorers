72a2a0a5899783ecc46c3a099e6550f312005875
NathanPhilliber <nphilliber@gmail.com>
2017-05-12 16:02:49 -0700

    Texture assembler overlays/filters and started terrain generation

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	//The how wide this chunk should be
+	public int width;
+
+	//How tall this chunk should be
+	public int height;
+
+	//How many platforms should be spawned?
+	public float density;
+
+	//The platform to be spawned (contains many variations within)
+	public GameObject platform;
+
+	//Where this section of platforms should be enterable
+	public int enterPointY;
+
+
+}
--- Assets/Scripts/TextureAssembler.cs
+++ Assets/Scripts/TextureAssembler.cs
@@ -18,6 +18,12 @@ public class TextureAssembler : MonoBehaviour {
 
 	private Color replacement = new Color (1, 0, 1, 1);
 
+	public const int TEXTUREMODE_NORMAL = 0;						//Display the image normally overlayed
+	public const int TEXTUREMODE_DISTORT_0 = 1;						//Display the image with offset squares
+	public const int TEXTUREMODE_DISTORT_1 = 2;						//Display the image with noise
+	public const int TEXTUREMODE_VERTICAL_OVERLAY_0 = 3;			//Display the image with second overlay randomly over it
+	public const int TEXTUREMODE_HORIZONTAL_OVERLAY_DISTORT_0 = 4;	//Display the image with second overlay randomly over it
+
 	/*******************************************************************************************************************************/
 
 
@@ -28,7 +34,7 @@ public class TextureAssembler : MonoBehaviour {
 		this.rawTextures = rawTextures;
 
 		GameObject spawned = (GameObject)Instantiate (cube, Vector3.zero, new Quaternion(0,0,180,0));
-		spawned.GetComponent<Renderer> ().material.mainTexture = SuperImposeTexture (texture, (Texture2D)rawTextures [0]);
+		spawned.GetComponent<Renderer> ().material.mainTexture = SuperImposeTexture (texture, (Texture2D)rawTextures [0], 4, (Texture2D)rawTextures[1]);
 		spawned.GetComponent<Renderer> ().material.shader = Shader.Find ("Unlit/Texture");
 	}
 
@@ -36,15 +42,67 @@ public class TextureAssembler : MonoBehaviour {
 	/*
 	 * Superimpose a texture onto another texture with magic pink values (255, 0, 255)
 	 */
-	public Texture2D SuperImposeTexture(Texture2D baseTex, Texture2D overlayTex){
+	public Texture2D SuperImposeTexture(Texture2D baseTex, Texture2D overlayTex, int mode = 0, Texture2D overlayTex2 = null){
 		Texture2D newTex = new Texture2D (baseTex.width, baseTex.height);
 
 		newTex.SetPixels32 (baseTex.GetPixels32 ());
 
-		for (int x = 0; x < newTex.width; x++) {
-			for (int y = 0; y < newTex.height; y++) {				
-				if (newTex.GetPixel (x, y) == replacement) {
-					newTex.SetPixel (x, y, GetSafePixel ((Texture2D)overlayTex, x, y));
+
+		if (mode == TEXTUREMODE_NORMAL) {
+			for (int x = 0; x < newTex.width; x++) {
+				for (int y = 0; y < newTex.height; y++) {				
+					if (newTex.GetPixel (x, y) == replacement) {
+						newTex.SetPixel (x, y, GetSafePixel ((Texture2D)overlayTex, x, y));
+					}
+				}
+			}
+		} else if (mode == TEXTUREMODE_DISTORT_0) {
+			for (int x = 0; x < newTex.width; x++) {
+				for (int y = 0; y < newTex.height; y++) {				
+					if (newTex.GetPixel (x, y) == replacement) {
+						if (x % 20 < 10 && y % 20 < 10) {
+							newTex.SetPixel (x, y, GetSafePixel ((Texture2D)overlayTex, x+50, y+50));
+						} else {
+							newTex.SetPixel (x, y, GetSafePixel ((Texture2D)overlayTex, x, y));
+						}
+					}
+				}
+			}
+		}
+		else if (mode == TEXTUREMODE_DISTORT_1) {
+			for (int x = 0; x < newTex.width; x++) {
+				for (int y = 0; y < newTex.height; y++) {				
+					if (newTex.GetPixel (x, y) == replacement) {
+						newTex.SetPixel (x, y, GetSafePixel ((Texture2D)overlayTex, x+ Random.Range(0,20), y+ Random.Range(0,20)));
+					}
+				}
+			}
+		}
+		else if (mode == TEXTUREMODE_HORIZONTAL_OVERLAY_DISTORT_0) {
+			for (int x = 0; x < newTex.width; x++) {
+				for (int y = 0; y < newTex.height; y++) {				
+					if (newTex.GetPixel (x, y) == replacement) {
+						if (y % 80 < 40) {
+							newTex.SetPixel (x, y, GetSafePixel ((Texture2D)overlayTex2, x, y));
+						} else {
+							newTex.SetPixel (x, y, GetSafePixel ((Texture2D)overlayTex, x, y));
+						}
+
+					}
+				}
+			}
+		}
+		else if (mode == TEXTUREMODE_VERTICAL_OVERLAY_0) {
+			for (int x = 0; x < newTex.width; x++) {
+				for (int y = 0; y < newTex.height; y++) {				
+					if (newTex.GetPixel (x, y) == replacement) {
+						if (x % 80 < 40) {
+							newTex.SetPixel (x, y, GetSafePixel ((Texture2D)overlayTex2, x, y));
+						} else {
+							newTex.SetPixel (x, y, GetSafePixel ((Texture2D)overlayTex, x, y));
+						}
+
+					}
 				}
 			}
 		}
